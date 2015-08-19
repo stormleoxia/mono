d8187ea322cafc63389c022ec748e7d9ce77b00b
storm <storm@leoxia.com>
2015-08-20 01:26:03 +0700

    Add System.Web.Entity
    # Conflicts:
    #	mcs/class/System.Drawing/System.Drawing-net_4_5.csproj
    #	mcs/class/System.Web.Extensions/System.Web.Extensions-net_4_5.csproj
    #	mcs/class/System/System-bare-net_4_5.csproj
    #	msvc/scripts/net_4_5.sln

---
+++ mcs/class/System.Web.Entity/System.Web.UI.WebControls/Strings.cs
@@ -0,0 +1,506 @@
+using System;
+namespace System.Web.UI.WebControls
+{
+	internal static class Strings
+	{
+		internal static string SetValueNotSupported
+		{
+			get
+			{
+				return WebControlsRes.GetString("SetValueNotSupported");
+			}
+		}
+		internal static string ComponentNotFromProperCollection
+		{
+			get
+			{
+				return WebControlsRes.GetString("ComponentNotFromProperCollection");
+			}
+		}
+		internal static string EntityDataSource_Description
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_Description");
+			}
+		}
+		internal static string EntityDataSource_DisplayName
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_DisplayName");
+			}
+		}
+		internal static string PropertyDescription_AutoGenerateOrderByClause
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_AutoGenerateOrderByClause");
+			}
+		}
+		internal static string PropertyDescription_AutoGenerateWhereClause
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_AutoGenerateWhereClause");
+			}
+		}
+		internal static string PropertyDescription_AutoPage
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_AutoPage");
+			}
+		}
+		internal static string PropertyDescription_AutoSort
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_AutoSort");
+			}
+		}
+		internal static string PropertyDescription_EnableFlattening
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_EnableFlattening");
+			}
+		}
+		internal static string PropertyDescription_EnableDelete
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_EnableDelete");
+			}
+		}
+		internal static string PropertyDescription_EnableInsert
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_EnableInsert");
+			}
+		}
+		internal static string PropertyDescription_EnableUpdate
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_EnableUpdate");
+			}
+		}
+		internal static string PropertyDescription_GroupBy
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_GroupBy");
+			}
+		}
+		internal static string PropertyDescription_Include
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Include");
+			}
+		}
+		internal static string PropertyDescription_ContextTypeName
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_ContextTypeName");
+			}
+		}
+		internal static string PropertyDescription_StoreOriginalValuesInViewState
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_StoreOriginalValuesInViewState");
+			}
+		}
+		internal static string PropertyDescription_ContextCreating
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_ContextCreating");
+			}
+		}
+		internal static string PropertyDescription_ContextCreated
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_ContextCreated");
+			}
+		}
+		internal static string PropertyDescription_ContextDisposing
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_ContextDisposing");
+			}
+		}
+		internal static string PropertyDescription_Selecting
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Selecting");
+			}
+		}
+		internal static string PropertyDescription_Selected
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Selected");
+			}
+		}
+		internal static string PropertyDescription_Deleting
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Deleting");
+			}
+		}
+		internal static string PropertyDescription_Deleted
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Deleted");
+			}
+		}
+		internal static string PropertyDescription_Inserting
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Inserting");
+			}
+		}
+		internal static string PropertyDescription_Inserted
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Inserted");
+			}
+		}
+		internal static string PropertyDescription_Updating
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Updating");
+			}
+		}
+		internal static string PropertyDescription_Updated
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_Updated");
+			}
+		}
+		internal static string PropertyDescription_QueryCreated
+		{
+			get
+			{
+				return WebControlsRes.GetString("PropertyDescription_QueryCreated");
+			}
+		}
+		internal static string EntityDataSource_CommandTextOrEntitySetName
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_CommandTextOrEntitySetName");
+			}
+		}
+		internal static string EntityDataSource_CommandTextOrEntitySetNameRequired
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_CommandTextOrEntitySetNameRequired");
+			}
+		}
+		internal static string EntityDataSource_SelectNotEditable
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_SelectNotEditable");
+			}
+		}
+		internal static string EntityDataSource_CommandTextNotEditable
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_CommandTextNotEditable");
+			}
+		}
+		internal static string EntityDataSource_GroupByNotEditable
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_GroupByNotEditable");
+			}
+		}
+		internal static string EntityDataSource_AutoGenerateWhereNotAllowedIfWhereDefined
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_AutoGenerateWhereNotAllowedIfWhereDefined");
+			}
+		}
+		internal static string EntityDataSource_AutoGenerateOrderByNotAllowedIfOrderByIsDefined
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_AutoGenerateOrderByNotAllowedIfOrderByIsDefined");
+			}
+		}
+		internal static string EntityDataSource_WhereParametersNeedsWhereOrAutoGenerateWhere
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_WhereParametersNeedsWhereOrAutoGenerateWhere");
+			}
+		}
+		internal static string EntityDataSource_OrderByParametersNeedsOrderByOrAutoGenerateOrderBy
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_OrderByParametersNeedsOrderByOrAutoGenerateOrderBy");
+			}
+		}
+		internal static string EntityDataSource_CommandParametersNeedCommandText
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_CommandParametersNeedCommandText");
+			}
+		}
+		internal static string EntityDataSource_SelectParametersNeedSelect
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_SelectParametersNeedSelect");
+			}
+		}
+		internal static string EntityDataSource_GroupByNeedsSelect
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_GroupByNeedsSelect");
+			}
+		}
+		internal static string EntityDataSource_CommandTextCantHaveEntityTypeFilter
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSource_CommandTextCantHaveEntityTypeFilter");
+			}
+		}
+		internal static string EntityDataSourceQueryBuilder_PagingRequiresOrderBy
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceQueryBuilder_PagingRequiresOrderBy");
+			}
+		}
+		internal static string EntityDataSourceView_UpdateDisabledForThisControl
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_UpdateDisabledForThisControl");
+			}
+		}
+		internal static string EntityDataSourceView_DeleteDisabledForThiscontrol
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_DeleteDisabledForThiscontrol");
+			}
+		}
+		internal static string EntityDataSourceView_InsertDisabledForThisControl
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_InsertDisabledForThisControl");
+			}
+		}
+		internal static string EntityDataSourceView_NoParameterlessConstructorForTheContext
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_NoParameterlessConstructorForTheContext");
+			}
+		}
+		internal static string EntityDataSourceView_ObjectContextMustBeSpecified
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_ObjectContextMustBeSpecified");
+			}
+		}
+		internal static string EntityDataSourceView_ContainerNameMustBeSpecified
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_ContainerNameMustBeSpecified");
+			}
+		}
+		internal static string EntityDataSourceView_ColumnHeader
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_ColumnHeader");
+			}
+		}
+		internal static string EntityDataSourceView_NoKeyProperty
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_NoKeyProperty");
+			}
+		}
+		internal static string EntityDataSourceView_AutoGenerateOrderByParameters
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_AutoGenerateOrderByParameters");
+			}
+		}
+		internal static string EntityDataSourceView_EmptyPropertyName
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceView_EmptyPropertyName");
+			}
+		}
+		internal static string EntityDataSourceUtil_EntityQueryCannotReturnPolymorphicTypes
+		{
+			get
+			{
+				return WebControlsRes.GetString("EntityDataSourceUtil_EntityQueryCannotReturnPolymorphicTypes");
+			}
+		}
+		internal static string WebControlParameterProxy_TypeDbTypeMutuallyExclusive
+		{
+			get
+			{
+				return WebControlsRes.GetString("WebControlParameterProxy_TypeDbTypeMutuallyExclusive");
+			}
+		}
+		internal static string EntityDataSourceView_EntitySetMismatchWithQueryResults(object p0, object p1)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_EntitySetMismatchWithQueryResults", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+		internal static string EntityDataSourceView_QueryCreatedNotAnObjectQuery(object p0, object p1)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_QueryCreatedNotAnObjectQuery", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+		internal static string EntityDataSourceView_QueryCreatedWrongType(object p0, object p1)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_QueryCreatedWrongType", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+		internal static string DefaultConstructorNotFound(object p0)
+		{
+			return WebControlsRes.GetString("DefaultConstructorNotFound", new object[]
+			{
+				p0
+			});
+		}
+		internal static string PropertyNotFound(object p0, object p1)
+		{
+			return WebControlsRes.GetString("PropertyNotFound", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+		internal static string ValueNotResettable(object p0)
+		{
+			return WebControlsRes.GetString("ValueNotResettable", new object[]
+			{
+				p0
+			});
+		}
+		internal static string DisplayNameCollision(object p0)
+		{
+			return WebControlsRes.GetString("DisplayNameCollision", new object[]
+			{
+				p0
+			});
+		}
+		internal static string EntityDataSourceView_PropertyDoesNotExistOnEntity(object p0, object p1)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_PropertyDoesNotExistOnEntity", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+		internal static string EntityDataSourceView_EntitySetDoesNotExistOnTheContainer(object p0)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_EntitySetDoesNotExistOnTheContainer", new object[]
+			{
+				p0
+			});
+		}
+		internal static string EntityDataSourceView_ContainerNameDoesNotExistOnTheContext(object p0)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_ContainerNameDoesNotExistOnTheContext", new object[]
+			{
+				p0
+			});
+		}
+		internal static string EntityDataSourceView_DataConversionError(object p0, object p1)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_DataConversionError", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+		internal static string EntityDataSourceView_FilteredEntityTypeMustBeDerivableFromEntitySet(object p0, object p1)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_FilteredEntityTypeMustBeDerivableFromEntitySet", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+		internal static string EntityDataSourceView_UnknownProperty(object p0)
+		{
+			return WebControlsRes.GetString("EntityDataSourceView_UnknownProperty", new object[]
+			{
+				p0
+			});
+		}
+		internal static string EntityDataSourceUtil_UnableToConvertTypeCodeToType(object p0)
+		{
+			return WebControlsRes.GetString("EntityDataSourceUtil_UnableToConvertTypeCodeToType", new object[]
+			{
+				p0
+			});
+		}
+		internal static string EntityDataSourceUtil_InsertUpdateParametersDontMatchPropertyNameOnEntity(object p0, object p1)
+		{
+			return WebControlsRes.GetString("EntityDataSourceUtil_InsertUpdateParametersDontMatchPropertyNameOnEntity", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+		internal static string EntityDataSourceUtil_UnableToConvertStringToType(object p0, object p1)
+		{
+			return WebControlsRes.GetString("EntityDataSourceUtil_UnableToConvertStringToType", new object[]
+			{
+				p0,
+				p1
+			});
+		}
+	}
+}
---
+++ mcs/class/System.Web.Entity/System.Web.UI/WebControlRes.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+using System.Threading;
+namespace System.Web.UI
+{
+    internal sealed class WebControlsRes
+    {
+        internal const string DefaultConstructorNotFound = "DefaultConstructorNotFound";
+        internal const string PropertyNotFound = "PropertyNotFound";
+        internal const string ValueNotResettable = "ValueNotResettable";
+        internal const string DisplayNameCollision = "DisplayNameCollision";
+        internal const string SetValueNotSupported = "SetValueNotSupported";
+        internal const string ComponentNotFromProperCollection = "ComponentNotFromProperCollection";
+        internal const string EntityDataSource_Description = "EntityDataSource_Description";
+        internal const string EntityDataSource_DisplayName = "EntityDataSource_DisplayName";
+        internal const string PropertyDescription_AutoGenerateOrderByClause = "PropertyDescription_AutoGenerateOrderByClause";
+        internal const string PropertyDescription_AutoGenerateWhereClause = "PropertyDescription_AutoGenerateWhereClause";
+        internal const string PropertyDescription_AutoPage = "PropertyDescription_AutoPage";
+        internal const string PropertyDescription_AutoSort = "PropertyDescription_AutoSort";
+        internal const string PropertyDescription_EnableFlattening = "PropertyDescription_EnableFlattening";
+        internal const string PropertyDescription_EnableDelete = "PropertyDescription_EnableDelete";
+        internal const string PropertyDescription_EnableInsert = "PropertyDescription_EnableInsert";
+        internal const string PropertyDescription_EnableUpdate = "PropertyDescription_EnableUpdate";
+        internal const string PropertyDescription_GroupBy = "PropertyDescription_GroupBy";
+        internal const string PropertyDescription_Include = "PropertyDescription_Include";
+        internal const string PropertyDescription_ContextTypeName = "PropertyDescription_ContextTypeName";
+        internal const string PropertyDescription_StoreOriginalValuesInViewState = "PropertyDescription_StoreOriginalValuesInViewState";
+        internal const string PropertyDescription_ContextCreating = "PropertyDescription_ContextCreating";
+        internal const string PropertyDescription_ContextCreated = "PropertyDescription_ContextCreated";
+        internal const string PropertyDescription_ContextDisposing = "PropertyDescription_ContextDisposing";
+        internal const string PropertyDescription_Selecting = "PropertyDescription_Selecting";
+        internal const string PropertyDescription_Selected = "PropertyDescription_Selected";
+        internal const string PropertyDescription_Deleting = "PropertyDescription_Deleting";
+        internal const string PropertyDescription_Deleted = "PropertyDescription_Deleted";
+        internal const string PropertyDescription_Inserting = "PropertyDescription_Inserting";
+        internal const string PropertyDescription_Inserted = "PropertyDescription_Inserted";
+        internal const string PropertyDescription_Updating = "PropertyDescription_Updating";
+        internal const string PropertyDescription_Updated = "PropertyDescription_Updated";
+        internal const string PropertyDescription_QueryCreated = "PropertyDescription_QueryCreated";
+        internal const string EntityDataSource_CommandTextOrEntitySetName = "EntityDataSource_CommandTextOrEntitySetName";
+        internal const string EntityDataSource_CommandTextOrEntitySetNameRequired = "EntityDataSource_CommandTextOrEntitySetNameRequired";
+        internal const string EntityDataSource_SelectNotEditable = "EntityDataSource_SelectNotEditable";
+        internal const string EntityDataSource_CommandTextNotEditable = "EntityDataSource_CommandTextNotEditable";
+        internal const string EntityDataSource_GroupByNotEditable = "EntityDataSource_GroupByNotEditable";
+        internal const string EntityDataSource_AutoGenerateWhereNotAllowedIfWhereDefined = "EntityDataSource_AutoGenerateWhereNotAllowedIfWhereDefined";
+        internal const string EntityDataSource_AutoGenerateOrderByNotAllowedIfOrderByIsDefined = "EntityDataSource_AutoGenerateOrderByNotAllowedIfOrderByIsDefined";
+        internal const string EntityDataSource_WhereParametersNeedsWhereOrAutoGenerateWhere = "EntityDataSource_WhereParametersNeedsWhereOrAutoGenerateWhere";
+        internal const string EntityDataSource_OrderByParametersNeedsOrderByOrAutoGenerateOrderBy = "EntityDataSource_OrderByParametersNeedsOrderByOrAutoGenerateOrderBy";
+        internal const string EntityDataSource_CommandParametersNeedCommandText = "EntityDataSource_CommandParametersNeedCommandText";
+        internal const string EntityDataSource_SelectParametersNeedSelect = "EntityDataSource_SelectParametersNeedSelect";
+        internal const string EntityDataSource_GroupByNeedsSelect = "EntityDataSource_GroupByNeedsSelect";
+        internal const string EntityDataSource_CommandTextCantHaveEntityTypeFilter = "EntityDataSource_CommandTextCantHaveEntityTypeFilter";
+        internal const string EntityDataSourceQueryBuilder_PagingRequiresOrderBy = "EntityDataSourceQueryBuilder_PagingRequiresOrderBy";
+        internal const string EntityDataSourceView_UpdateDisabledForThisControl = "EntityDataSourceView_UpdateDisabledForThisControl";
+        internal const string EntityDataSourceView_DeleteDisabledForThiscontrol = "EntityDataSourceView_DeleteDisabledForThiscontrol";
+        internal const string EntityDataSourceView_InsertDisabledForThisControl = "EntityDataSourceView_InsertDisabledForThisControl";
+        internal const string EntityDataSourceView_NoParameterlessConstructorForTheContext = "EntityDataSourceView_NoParameterlessConstructorForTheContext";
+        internal const string EntityDataSourceView_ObjectContextMustBeSpecified = "EntityDataSourceView_ObjectContextMustBeSpecified";
+        internal const string EntityDataSourceView_ContainerNameMustBeSpecified = "EntityDataSourceView_ContainerNameMustBeSpecified";
+        internal const string EntityDataSourceView_PropertyDoesNotExistOnEntity = "EntityDataSourceView_PropertyDoesNotExistOnEntity";
+        internal const string EntityDataSourceView_EntitySetDoesNotExistOnTheContainer = "EntityDataSourceView_EntitySetDoesNotExistOnTheContainer";
+        internal const string EntityDataSourceView_EntitySetMismatchWithQueryResults = "EntityDataSourceView_EntitySetMismatchWithQueryResults";
+        internal const string EntityDataSourceView_ContainerNameDoesNotExistOnTheContext = "EntityDataSourceView_ContainerNameDoesNotExistOnTheContext";
+        internal const string EntityDataSourceView_ColumnHeader = "EntityDataSourceView_ColumnHeader";
+        internal const string EntityDataSourceView_DataConversionError = "EntityDataSourceView_DataConversionError";
+        internal const string EntityDataSourceView_FilteredEntityTypeMustBeDerivableFromEntitySet = "EntityDataSourceView_FilteredEntityTypeMustBeDerivableFromEntitySet";
+        internal const string EntityDataSourceView_QueryCreatedNotAnObjectQuery = "EntityDataSourceView_QueryCreatedNotAnObjectQuery";
+        internal const string EntityDataSourceView_QueryCreatedWrongType = "EntityDataSourceView_QueryCreatedWrongType";
+        internal const string EntityDataSourceView_UnknownProperty = "EntityDataSourceView_UnknownProperty";
+        internal const string EntityDataSourceView_NoKeyProperty = "EntityDataSourceView_NoKeyProperty";
+        internal const string EntityDataSourceView_AutoGenerateOrderByParameters = "EntityDataSourceView_AutoGenerateOrderByParameters";
+        internal const string EntityDataSourceView_EmptyPropertyName = "EntityDataSourceView_EmptyPropertyName";
+        internal const string EntityDataSourceUtil_UnableToConvertTypeCodeToType = "EntityDataSourceUtil_UnableToConvertTypeCodeToType";
+        internal const string EntityDataSourceUtil_EntityQueryCannotReturnPolymorphicTypes = "EntityDataSourceUtil_EntityQueryCannotReturnPolymorphicTypes";
+        internal const string EntityDataSourceUtil_InsertUpdateParametersDontMatchPropertyNameOnEntity = "EntityDataSourceUtil_InsertUpdateParametersDontMatchPropertyNameOnEntity";
+        internal const string EntityDataSourceUtil_UnableToConvertStringToType = "EntityDataSourceUtil_UnableToConvertStringToType";
+        internal const string WebControlParameterProxy_TypeDbTypeMutuallyExclusive = "WebControlParameterProxy_TypeDbTypeMutuallyExclusive";
+        private static WebControlsRes loader;
+        private ResourceManager resources;
+        private static CultureInfo Culture
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public static ResourceManager Resources
+        {
+            get
+            {
+                return WebControlsRes.GetLoader().resources;
+            }
+        }
+        internal WebControlsRes()
+        {
+            this.resources = new ResourceManager("System.Web.UI.WebControls", base.GetType().Assembly);
+        }
+        private static WebControlsRes GetLoader()
+        {
+            if (WebControlsRes.loader == null)
+            {
+                WebControlsRes value = new WebControlsRes();
+                Interlocked.CompareExchange<WebControlsRes>(ref WebControlsRes.loader, value, null);
+            }
+            return WebControlsRes.loader;
+        }
+        public static string GetString(string name, params object[] args)
+        {
+            WebControlsRes webControlsRes = WebControlsRes.GetLoader();
+            if (webControlsRes == null)
+            {
+                return null;
+            }
+            string @string = webControlsRes.resources.GetString(name, WebControlsRes.Culture);
+            if (args != null && args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string text = args[i] as string;
+                    if (text != null && text.Length > 1024)
+                    {
+                        args[i] = text.Substring(0, 1021) + "...";
+                    }
+                }
+                return string.Format(CultureInfo.CurrentCulture, @string, args);
+            }
+            return @string;
+        }
+        public static string GetString(string name)
+        {
+            WebControlsRes webControlsRes = WebControlsRes.GetLoader();
+            if (webControlsRes == null)
+            {
+                return null;
+            }
+            return webControlsRes.resources.GetString(name, WebControlsRes.Culture);
+        }
+        public static string GetString(string name, out bool usedFallback)
+        {
+            usedFallback = false;
+            return WebControlsRes.GetString(name);
+        }
+        public static object GetObject(string name)
+        {
+            WebControlsRes webControlsRes = WebControlsRes.GetLoader();
+            if (webControlsRes == null)
+            {
+                return null;
+            }
+            return webControlsRes.resources.GetObject(name, WebControlsRes.Culture);
+        }
+    }
+}
