737a8222e80bd354241036078441f793892a296f
storm <storm@leoxia.com>
2015-08-20 01:26:03 +0700

    Add System.Web.Entity

---
+++ mcs/class/System.Drawing/System.Drawing/BitmapSuffixInSameAssemblyAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Drawing
+{
+    /// <summary>
+    /// Specifies that, when interpreting <see cref="T:System.Drawing.ToolboxBitmapAttribute"/> declarations, the assembly should look for the indicated resources in the same assembly, but with the <see cref="P:System.Drawing.Configuration.SystemDrawingSection.BitmapSuffix"/> configuration value appended to the declared file name.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class BitmapSuffixInSameAssemblyAttribute : Attribute
+    {
+    }
+}
